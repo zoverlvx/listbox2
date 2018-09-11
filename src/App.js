1595b88c7f9a4b25f72ba12873b018d2e11f2d22
zoverlvx <zoverhulser@gmail.com>
2018-09-10 23:41:42 -0400

    Establishes state.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            
+        }
+    }
 
     newItemChanged(e) {
         console.log(e.target.value)
