f5aecf57258ff071fe27d906ff6adee7db1cc448
zoverlvx <zoverhulser@gmail.com>
2018-09-11 00:43:14 -0400

    Defines itemsInCart property as empty array within the state.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
         super();
         this.state = {
             newItem: "",
-            itemsToBuy: []
+            itemsToBuy: [],
+            itemsInCart: []
         }
     }
 
