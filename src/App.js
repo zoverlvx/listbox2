8317ffd0cdad1fc9cd5a2369e34df59c298eda48
zoverlvx <zoverhulser@gmail.com>
2018-09-10 23:25:29 -0400

    Sets break between label and input box for aesthetic purposes.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
                     >
                         New Item
                     </label>
+                    <br />
                     <input 
                         id="newItem"
                         name="newItem"
